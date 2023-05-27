403eb3675fdaccdca76affa105817c83eb3c86a7
dogakocak <dogakocak>
2023-05-28 00:19:13 +0300

    AddEditContactActivity is implemented. Some vectors added.

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addPersonButton(View view){
+        Intent intent = new Intent(this, AddEditContactActivity.class);
+        startActivity(intent);
     }
 }
